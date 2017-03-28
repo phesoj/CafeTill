427e34fa59f160c1da55e32c10433fcc98209c77
Joseph Griffiths <dana@Josephs-MBP.home>
2017-03-28 16:24:32 +0100

    Step2: Adding service charge

--- src/main/scala/CafeTill.scala
+++ src/main/scala/CafeTill.scala
@@ -1,7 +1,23 @@
-case class Food(name: String, temperature: String, price: BigDecimal)
-
+case class Food(name: String, temperature: String, category: String, price: BigDecimal)
 
 object CafeTill {
-  def totalBill(items: Seq[Food]) = items.map(_.price).sum
+  val MaxServiceCharge = 20.00
+
+  def totalBill(items: Seq[Food]) = {
+    val subTotal = items.map(_.price).sum
+    val rateToAdd = items.map(serviceChargeRate).max
+    val rated = subTotal * rateToAdd
+
+    val fee: BigDecimal = if (rated > MaxServiceCharge) MaxServiceCharge  else rated
+
+      subTotal + fee
+  }
+
+  private def serviceChargeRate(item: Food):BigDecimal =
+    if (item.category == "Food" && item.temperature == "Hot") .20
+    else if (item.category == "Food") .10
+    else .0
 }
+
+
 
--- src/test/scala/CafeTillSpec.scala
+++ src/test/scala/CafeTillSpec.scala
@@ -3,15 +3,29 @@ import org.scalatest.Matchers._
 
 class CafeTillSpec extends FunSpec {
 
-  val Cola = Food("Cola", "Cold", .50)
-  val Coffee = Food("Coffee", "Hot", 1.00)
-  val CheeseSandwich = Food("Cheese Sandwich", "Cold", 2.00)
-  val SteakSandwich = Food("Steak Sandwich", "Hot", 4.50)
+  val Cola = Food("Cola", "Cold", "Drink", .50)
+  val Coffee = Food("Coffee", "Hot", "Drink", 1.00)
+  val CheeseSandwich = Food("Cheese Sandwich", "Cold","Food", 2.00)
+  val SteakSandwich = Food("Steak Sandwich","Hot", "Food", 4.50)
+  val veryExpensiveItem = Food("Lobster for 10", "Hot", "Food", 200)
 
   describe("the Cafe Till") {
     it("should work out the cost of the items purchased") {
-      CafeTill.totalBill(Seq(Cola, Coffee, CheeseSandwich)) shouldEqual 3.5
+      CafeTill.totalBill(Seq(Cola, Coffee)) shouldEqual 1.5
     }
-  }
 
+    it("should add service charge of 10% when food is included") {
+      CafeTill.totalBill(Seq(Cola, Coffee, CheeseSandwich)) shouldEqual 3.85
+    }
+
+    it("should add service charge of 20% when Hot food is included") {
+      CafeTill.totalBill(Seq(SteakSandwich)) shouldEqual 5.40
+      CafeTill.totalBill(Seq(Coffee)) shouldEqual 1.00
+    }
+
+    it("should add service charge of no more than £20") {
+      CafeTill.totalBill(Seq(veryExpensiveItem)) shouldEqual 220
+    }
+
+  }
 }
